5d766f81ee1faa26759c64988e66de8ca0b0cc1c
liuxiang <liuxiang@aiyunxiao.com>
2020-08-24 10:31:50 +0800

    创建todo项目

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import CompositionApi from '@vue/composition-api'
+
 
 Vue.config.productionTip = false;
-
+Vue.use(CompositionApi)
 new Vue({
   router,
   store,
